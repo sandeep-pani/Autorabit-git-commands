993a41044184ad5c49c4d6322b9be387f2a2b3ef
Sandeep Pani <sandeep.pani@hotmail.com>
2023-04-13 16:49:03 +0530

    added missing space

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("Hello World");
     }
 }
